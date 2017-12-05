56b9b5e4d76a98de57b0379c6a717b51a4e52037
Nikita Chebykin <nikita@chebyk.in>
2017-12-05 21:04:27 +0200

    Add GitHub authentication

--- config/config.exs
+++ config/config.exs
@@ -25,3 +25,12 @@ config :logger, :console,
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
+
+config :ueberauth, Ueberauth,
+  providers: [
+    github: {Ueberauth.Strategy.Github, []}
+  ]
+
+config :ueberauth, Ueberauth.Strategy.Github.OAuth,
+  client_id: "de0016b58788f8d37897",
+  client_secret: "b0dfd6dbde7b35b20e6c354f948778e816fcbeb2"
